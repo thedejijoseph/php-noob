87bc3393e6c088599834c9a347608ad78ebcd542
Deji <wrecodde@gmail.com>
2021-05-07 21:28:44 +0100

    working conditionals

---
+++ conditionals.php
@@ -0,0 +1,17 @@
+<?php
+
+$target = 6;
+
+switch ($target){
+    case $target > 12:
+        echo "Works here \n";
+        // break;
+    case $target > 14:
+        echo "And here \n";
+        // break;
+    default:
+        echo "The night after \n";
+        // break;
+}
+
+?>
